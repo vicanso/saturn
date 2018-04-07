f6119a547b9b4bb51606b5d06c959e2df944e3f5
vicanso <vicansocanbico@gmail.com>
2018-04-07 18:53:00 +0800

    chore: 增加构建APP版本的脚本

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const webpack = require('webpack');
+
+const target = process.env.TARGET;
+
+const output = {};
+let urlPrefix = '/api';
+
+if (target === 'app') {
+  output.publicPath = './';
+  urlPrefix = 'http://xs.aslant.site/api';
+}
+
 module.exports = {
   devServer: {
     proxy: {
@@ -9,11 +20,12 @@ module.exports = {
     },
   },
   configureWebpack: {
+    output,
     plugins: [
       new webpack.DefinePlugin({
         ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
         APP: JSON.stringify('novel'),
-        URL_PREFIX: JSON.stringify('/api'),
+        URL_PREFIX: JSON.stringify(urlPrefix),
       }),
     ],
   },
